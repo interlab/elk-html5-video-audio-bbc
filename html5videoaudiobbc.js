ac38d383fb8e3a3ab00d24b05f1a98270a1fc97a
inter <pmrtorrents@gmail.com>
2017-04-26 16:42:39 +0300

    + enable in wysiwyg mode
    Signed-off-by: inter <pmrtorrents@gmail.com>

--- html5videoaudiobbc.js
+++ html5videoaudiobbc.js
@@ -1,10 +1,12 @@
-// What to add in the editor window when the button is clicked, based on which mode the editor is in
+;
 $.sceditor.command
 	.set("html5video", {
+		exec: function(caller){ this.insert("[html5video]", "[/html5video]", false); },
 		txtExec: ["[html5video]", "[/html5video]"],
 		tooltip: "HTML5Video"
 	})
     .set("html5audio", {
+		exec: function(caller){ this.insert("[html5audio]", "[/html5audio]", false); },
 		txtExec: ["[html5audio]", "[/html5audio]"],
 		tooltip: "HTML5Audio"
 	}
