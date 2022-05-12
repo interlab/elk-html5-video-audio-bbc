b832791f170e384772d81842ab8e95fce0729677
inter <ytkin3000@gmail.com>
2022-05-12 15:22:53 +0300

    minor changes
    Signed-off-by: inter <ytkin3000@gmail.com>

--- elk-1.1/HTML5VideoAudioBBC.integrate.php
+++ elk-1.1/HTML5VideoAudioBBC.integrate.php
@@ -23,27 +23,22 @@ class HTML5VideoAudioBBC
             BBC\Codes::ATTR_TYPE => BBC\Codes::TYPE_UNPARSED_CONTENT,
             BBC\Codes::ATTR_CONTENT => '$1',
             BBC\Codes::ATTR_VALIDATE => function(&$tag, &$data, $disabled) {
+                if (in_array('html5audio', $disabled)) {
+                    return;
+                }
+
                 global $txt;
 
                 $link = $data;
 
-                if (in_array('html5audio', $disabled)) {
-                    $data = '[html5audio]' . $data . '[/html5audio]';
-                } elseif (empty($data)) {
-                    $data = '[html5audio]' . $data . '[/html5audio]';
-                } elseif (!in_array(strrchr($data, '.'), ['.mp3', '.ogg', '.wav'])) {
-                    $data = '[html5audio]' . $data . '[/html5audio]';
-                } else {
+                if (!empty($data) && in_array(strrchr($data, '.'), ['.mp3', '.ogg', '.wav'])) {
                     $type = HTML5VideoAudioBBC::getHtml5MediaType($data, 'audio');
                     $data = 'src="' . $data . '" type="' . $type . '"';
-                    $data = '
-<audio controls>
-    <source ' . $data . '>
-    ' . sprintf($txt['html5audiobbc_not_support'], $link) . '<br>
-</audio>';
+                    $data = '<audio controls><source ' . $data . '><p>' .
+                        sprintf($txt['html5audiobbc_not_support'], $link) . '</p></audio>';
                 }
             },
-            // BBC\Codes::ATTR_DISABLED_CONTENT => '($1)',
+            BBC\Codes::ATTR_DISABLED_CONTENT => '[html5audio]$1[/html5audio]',
             BBC\Codes::ATTR_BLOCK_LEVEL => false,
             BBC\Codes::ATTR_AUTOLINK => false,
             BBC\Codes::ATTR_LENGTH => 10,
@@ -54,27 +49,23 @@ class HTML5VideoAudioBBC
             BBC\Codes::ATTR_TYPE => BBC\Codes::TYPE_UNPARSED_CONTENT,
             BBC\Codes::ATTR_CONTENT => '$1',
             BBC\Codes::ATTR_VALIDATE => function(&$tag, &$data, $disabled) {
+                if (in_array('html5video', $disabled)) {
+                    return;
+                }
+
                 global $txt;
 
                 $link = $data;
 
-                if (in_array('html5video', $disabled)) {
-                    $data = '[html5video]' . $data . '[/html5video]';
-                } elseif (empty($data)) {
-                    $data = '[html5video]' . $data . '[/html5video]';
-                } elseif (!in_array(strrchr($data, '.'), ['.mp4', '.webm', '.ogg', '.ogv'])) {
-                    $data = '[html5video]' . $data . '[/html5video]';
-                } else {
+                if (!empty($data) && in_array(strrchr($data, '.'), ['.mp4', '.webm', '.ogg', '.ogv'])) {
                     $type = HTML5VideoAudioBBC::getHtml5MediaType($data, 'video');
                     $data = 'src="' . $data . '" type="' . $type . '"';
-                    $data = '
-<video class="elk-html5videoaudiobbc" controls>
-    <source ' . $data . '>
-    ' . sprintf($txt['html5videobbc_not_support'], $link) . '<br>
-</video>';
+                    $data = '<video class="elk-html5videoaudiobbc" controls>'.
+                        '<source ' . $data . '><p>' .
+                        sprintf($txt['html5videobbc_not_support'], $link) . '</p></video>';
                 }
             },
-            // BBC\Codes::ATTR_DISABLED_CONTENT => '($1)',
+            BBC\Codes::ATTR_DISABLED_CONTENT => '[html5video]$1[/html5video]',
             BBC\Codes::ATTR_BLOCK_LEVEL => false,
             BBC\Codes::ATTR_AUTOLINK => false,
             BBC\Codes::ATTR_LENGTH => 10,
@@ -88,7 +79,7 @@ class HTML5VideoAudioBBC
 
         $where = $bbc_tags['row2'][3];
         // And here we insert the new value after code
-        $bbc_tags['row2'][3] = elk_array_insert($where, 'link', array('html5video'), 'after', false);
+        $bbc_tags['row2'][3] = elk_array_insert($where, 'image', array('html5video'), 'after', false);
 
         $where = $bbc_tags['row2'][3];
         // And here we insert the new value after code
@@ -100,38 +91,19 @@ class HTML5VideoAudioBBC
     static public function getHtml5MediaType($data, $av)
     {
         $ext = strrchr($data, '.');
-        if (!$ext)
-            return 'text/plain';
-
-        if ('audio' === $av) {
-            if ('.mp3' === $ext)
-                return 'audio/mpeg';
-            elseif ('.ogg' === $ext)
-                return 'audio/ogg';
-            elseif ('.wav' === $ext)
-                return 'audio/wav';
-            else
-                return 'text/plain';
-        } elseif ('video' === $av) {
-            if ('.mp4' === $ext)
-                return 'video/mp4';
-            elseif ('.webm' === $ext)
-                return 'video/webm';
-            elseif ('.ogg' === $ext)
-                return 'video/ogg';
-            elseif ('.ogv' === $ext)
-                return 'video/ogg';
-            else
-                return 'text/plain';
-        } else {
+        if (!$ext) {
             return 'text/plain';
         }
 
-        /*
-        $ext = strtolower(array_pop(explode('.', $filename)));
-        if (array_key_exists($ext, $mime_types)) {
-            return $mime_types[$ext];
-        }
-         */
+        $ext = substr($ext, 1);
+
+        $arr = [
+            'audio' => ['mp3' => 'audio/mpeg', 'ogg' => 'audio/ogg', 'wav' => 'audio/wav'],
+            'video' => [
+                'mp4' => 'video/mp4', 'webm' => 'video/webm', 'ogg' => 'video/ogg', 'ogv' => 'video/ogg',
+             ]
+        ];
+
+        return isset($arr[$av][$ext]) ? $arr[$av][$ext] : 'text/plain';
     }
 }
--- languages/HTML5VideoAudioBBC.russian.php
+++ languages/HTML5VideoAudioBBC.russian.php
@@ -1,4 +1,4 @@
 <?php
 
-$txt['html5videobbc_not_support'] = 'Тег video не поддерживается вашим браузером. <a href="%s">~ Ссылка на видео ~</a>.';
-$txt['html5audiobbc_not_support'] = 'Тег audio не поддерживается вашим браузером. <a href="%s">~ Скачайте песню ~</a>.';
+$txt['html5videobbc_not_support'] = 'Тег video не поддерживается вашим браузером. <a href="%s">~ Ссылка на видео файл ~</a>';
+$txt['html5audiobbc_not_support'] = 'Тег audio не поддерживается вашим браузером. <a href="%s">~ Ссылка на аудио файл ~</a>';
